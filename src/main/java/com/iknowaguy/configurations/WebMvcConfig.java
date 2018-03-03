78f75d5e9b3dd287a20bc68b352a3533c4264968
Unknown <BuyaNami@Gmail.com>
2018-03-03 05:39:08 -0500

    .

--- src/main/java/com/iknowaguy/configurations/WebMvcConfig.java
+++ src/main/java/com/iknowaguy/configurations/WebMvcConfig.java
@@ -12,5 +12,44 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/public/" };
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//        // to serve static .html pages...
+//        registry.addResourceHandler("/static/**").addResourceLocations("../resources/static/");
+//        // to serve jsx components
+//        registry.addResourceHandler("/ui/**").addResourceLocations("/ui/");
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+//        if (!registry.hasMappingForPattern("/webjars/**")) {
+//            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        }
+//        if (!registry.hasMappingForPattern("/**")) {
+//            registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+//        }
+//    }
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("forward:/index.html");
+//    }
+
+//    // During development, webpack server runs on localhost:8080
+//    // Make the browser happy by returning CORS headers in this case
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
+//            }
+//        };
+//    }
 
 }
